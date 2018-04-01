0c55353d7087c4d7db9a449cc1e5cf1f0e4a1d11
Tien Dinh <devtyger@gmail.com>
2018-03-31 20:47:19 -0400

    Add ball

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var ballX = 75;
+
 var canvas, canvasContext;
 
 window.onload = function() {
@@ -16,9 +18,17 @@ function updateAll() {
 
 function drawAll() {
   colorRect(0,0, canvas.width, canvas.height, 'black'); 
+  colorCircle(ballX, 200, 10, 'cyan'); 
 }
 
 function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
   canvasContext.fillStyle = fillColor;
   canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+}
+
+function colorCircle(centerX, centerY, radius, fillColor) {
+  canvasContext.fillStyle = fillColor;
+  canvasContext.beginPath();
+  canvasContext.arc(centerX,centerY, radius, 0,Math.PI*2, true);
+  canvasContext.fill();
 }
